75b7d535683b156951a9c4178daa5e1e343a1329
tubi55 <tubi55@nate.com>
2022-08-19 14:12:34 +0900

    youtube전용 리듀서, 액션생성함수 추가

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -35,8 +35,6 @@ const initMember = {
 	],
 };
 
-//초기데이터를 state에 저장했다가 추후 action객체가 전달되면
-//action객체의 타입에 따라 기존 데이터를 변경해서 리턴
 const memberReducer = (state = initMember, action) => {
 	switch (action.type) {
 		case 'SET_MEMBERS':
@@ -47,7 +45,15 @@ const memberReducer = (state = initMember, action) => {
 	}
 };
 
-//전달된 각각의 reducer를 하나로 합쳐서 반환
-const reducers = combineReducers({ memberReducer });
+const youtubeReducer = (state = { youtube: [] }, action) => {
+	switch (action.type) {
+		case 'SET_YOUTUBE':
+			return { ...state, youtube: action.payload };
+		default:
+			return state;
+	}
+};
+
+const reducers = combineReducers({ memberReducer, youtubeReducer });
 
 export default reducers;
